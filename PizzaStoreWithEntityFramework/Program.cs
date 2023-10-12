f07dd64482ff836489d4f16e8552da710e2b2afe
willvarner <96070490+willvarner@users.noreply.github.com>
2023-10-12 01:33:52 -0400

    task: bug hunting
    dammit

---
+++ PizzaStoreWithEntityFramework/Program.cs
@@ -0,0 +1,28 @@
+
+using Microsoft.OpenApi.Models;
+using PizzaStoreWithEntityFramework.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddDbContext<PizzaDb>(options => options.UseInMemoryDatabase("Items"));
+
+builder.Services.AddSwaggerGen(c => {
+    c.SwaggerDoc("v1", new OpenApiInfo {
+        Title = "PizzaStore API",
+        Description = "Making the pizzas you love.",
+        Version = "v1"
+    });
+});
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI(c => 
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "PizzaStore API V1");
+});
+
+app.MapGet("/pizzas", async (PizzaDb db) => await db.Pizzas.ToListAsync());
+app.Run();
