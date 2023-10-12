2dd3c1e82f70ee9c7a4cf07c5129a6eae2ea21e4
willvarner <96070490+willvarner@users.noreply.github.com>
2023-10-12 01:43:53 -0400

    fix: removed typo comma
    comma

--- PizzaStoreWithEntityFramework/Pizza.cs
+++ PizzaStoreWithEntityFramework/Pizza.cs
@@ -1,9 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace PizzaStoreWithEntityFramework.Models
 {
     public class Pizza
     {
-          public int Id { get; set; }
-          public string? Name { get; set; }
-          public string? Description { get; set; }
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+    }
+
+    internal class PizzaDb : DbContext
+    {
+        public PizzaDb(DbContextOptions options) : base(options) { }
+        public DbSet<Pizza> Pizzas { get; set; } = null!;
     }
 }
--- PizzaStoreWithEntityFramework/Program.cs
+++ PizzaStoreWithEntityFramework/Program.cs
@@ -1,4 +1,5 @@
 
+using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 using PizzaStoreWithEntityFramework.Models;
 
